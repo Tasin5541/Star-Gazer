47835589a30475f38738260b85f623ed201ccea6
Tasin5541 <tasinmiftaulmannan@gmail.com>
2021-01-26 18:43:16 +0600

    initial

---
+++ src/components/Game.js
@@ -0,0 +1,128 @@
+import React, { useState, useEffect } from "react";
+import utils from "../math-utils";
+import useGameState from "../customHooks/useGameState";
+
+import StarsDisplay from "./StarsDisplay";
+import PlayNumber from "./PlayNumber";
+import PlayAgain from "./PlayAgain";
+
+// const useGameState = () => {
+//   const [stars, setStars] = useState(utils.random(1, 9));
+//   const [availableNums, setAvailableNums] = useState(utils.range(1, 9));
+//   const [candidateNums, setCandidateNums] = useState([]);
+//   const [secondsLeft, setSecondsLeft] = useState(10);
+
+//   useEffect(() => {
+//     if (secondsLeft > 0 && availableNums.length > 0) {
+//       const timerId = setTimeout(
+//         () => setSecondsLeft((prevSecondsLeft) => prevSecondsLeft - 1),
+//         1000
+//       );
+//       return () => clearTimeout(timerId);
+//     }
+//   }, [secondsLeft, availableNums]);
+
+//   const setGameState = (newCandidateNums) => {
+//     if (utils.sum(newCandidateNums) !== stars) {
+//       setCandidateNums(newCandidateNums);
+//     } else {
+//       const newAvailableNums = availableNums.filter(
+//         (n) => !newCandidateNums.includes(n)
+//       );
+//       setStars(utils.randomSumIn(newAvailableNums, 9));
+//       setAvailableNums(newAvailableNums);
+//       setCandidateNums([]);
+//     }
+//   };
+
+//   return { stars, availableNums, candidateNums, secondsLeft, setGameState };
+// };
+
+const Game = (props) => {
+  const {
+    stars,
+    availableNums,
+    candidateNums,
+    secondsLeft,
+    setGameState,
+  } = useGameState();
+
+  const candidatesAreWrong = utils.sum(candidateNums) > stars;
+  const gameStatus =
+    availableNums.length === 0 ? "won" : secondsLeft === 0 ? "lost" : "active";
+
+  const numberStatus = (number) => {
+    if (!availableNums.includes(number)) {
+      return "used";
+    }
+
+    if (candidateNums.includes(number)) {
+      return candidatesAreWrong ? "wrong" : "candidate";
+    }
+
+    return "available";
+  };
+
+  const onNumberClick = (number, currentStatus) => {
+    if (currentStatus === "used" || secondsLeft === 0) {
+      return;
+    }
+
+    const newCandidateNums =
+      currentStatus === "available"
+        ? candidateNums.concat(number)
+        : candidateNums.filter((cn) => cn !== number);
+
+    setGameState(newCandidateNums);
+  };
+
+  return (
+    <div className="game">
+      <div className="help">
+        Pick 1 or more numbers that sum to the number of stars
+      </div>
+      <div className="body">
+        <div className="left">
+          {gameStatus !== "active" ? (
+            <PlayAgain onClick={props.startNewGame} gameStatus={gameStatus} />
+          ) : (
+            <StarsDisplay count={stars} />
+          )}
+        </div>
+        <div className="right">
+          {utils.range(1, 9).map((number) => (
+            <PlayNumber
+              key={number}
+              status={numberStatus(number)}
+              number={number}
+              onClick={onNumberClick}
+            />
+          ))}
+        </div>
+      </div>
+      <div className="timer">Time Remaining: {secondsLeft}</div>
+      <div>
+        <p>
+          <strong>Rules:</strong>
+        </p>
+        <ul>
+          <li>
+            Select a number that sums upto the numbers of stars displayed on the
+            left
+          </li>
+          <li>Example: 9 stars = 9 or (8+1) or (5+4)</li>
+          <li>
+            If the sum of numbers exceeds the total star count, the number will
+            turn red
+          </li>
+          <li>
+            You can de-select a number if it's red or blue. You can't de-select
+            a green number
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Game;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD-hVnRdHJMSk0eAsjqbcQlTVsTYBfgdRs",
+  authDomain: "star-gazer-93c9a.firebaseapp.com",
+  projectId: "star-gazer-93c9a",
+  storageBucket: "star-gazer-93c9a.appspot.com",
+  messagingSenderId: "102046357406",
+  appId: "1:102046357406:web:4fe65446987df4fedf288f",
+  measurementId: "G-0NXDX03CL9",
+};
